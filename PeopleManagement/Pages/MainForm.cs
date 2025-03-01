f3702ee83ae545943f171c23afe32c0ea946dbb9
Admin <eangsovannroth@gmail.com>
2025-03-01 23:35:46 +0700

    1.2: Connected to sqlite with minimum usage

--- PeopleManagement/Pages/MainForm.cs
+++ PeopleManagement/Pages/MainForm.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.Sqlite;
 using PeopleManagement.Models;
 using PeopleManagement.SortingControls;
 
@@ -11,10 +12,13 @@ public partial class MainForm : Form
         new NameSorter(),
         new AgeSorter(),
     };
+    private string _connection = "Data source=app.db";
+
 
     public MainForm()
     {
         InitializeComponent();
+        InitializeDatabase().Wait();
 
         _bindingSource.DataSource = Program.People;
         dgvPers.DataSource = _bindingSource;
@@ -23,22 +27,83 @@ public partial class MainForm : Form
 
         Loading();
 
-        cbSort.SelectedIndexChanged += (sender, e) =>
-        {
-            Sorted();
-        };
+        cbSort.SelectedIndexChanged += (sender, e) => Sorted();
         btnNew.Click += OnClickNew;
         btnEdit.Click += OnClickEdit;
         btnDelete.Click += OnClickDelete;
     }
-   
-    public void Loading()
+
+    #region Start-up
+    private async Task InitializeDatabase()
     {
-        dgvPers.AutoGenerateColumns = false;
+        try
+        {
+            await using (var conn = new SqliteConnection(_connection))
+        {
+            await conn.OpenAsync();
+
+            //First check if the table exists
+            await using (var cmdCheckTable = new SqliteCommand(
+                "SELECT name FROM sqlite_master WHERE type='table' AND name='People';", conn))
+            {
+                var result = await cmdCheckTable.ExecuteScalarAsync();
+
+                // If table doesn't exist, create it
+                if (result == null)
+                {
+                    await using (var cmdCreateTable = new SqliteCommand(
+                        @"CREATE TABLE People (
+                        No INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Name TEXT NOT NULL,
+                        Age INTEGER NOT NULL
+                    );", conn))
+                    {
+                        await cmdCreateTable.ExecuteNonQueryAsync();
+                    }
+
+                    await using (var insertSample = new SqliteCommand(
+                        @"INSERT INTO People (Name, Age) VALUES
+                            ('First', 1),
+                            ('Second', 2);", conn))
+                    {
+                        await insertSample.ExecuteNonQueryAsync();
+                    };
+                }
+            }
+
+            //Get people from the table if it exists
+            await using (var cmd = new SqliteCommand(
+                "SELECT No, Name, Age FROM People;", conn))
+            {
+
+                await using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        Program.People.Add(new Person
+                        {
+                            No = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Age = reader.GetInt32(2)
+                        });
+                    }
+                }
+            };
+            
+        }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show($"Error: {e.Message}", "Error", MessageBoxButtons.OK);
+        }
+    }
+    private void Loading()
+    {
         dgvPers.RowHeadersVisible = false;
         dgvPers.AllowUserToResizeColumns = false;
         dgvPers.AllowUserToResizeRows = false;
         dgvPers.AllowUserToAddRows = false;
+        dgvPers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
 
         dgvPers.AutoResizeColumns();
         dgvPers.ColumnHeadersHeight = 23;
@@ -50,7 +115,7 @@ public partial class MainForm : Form
         dgvPers.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
         dgvPers.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
     }
-    public void Sorted()
+    private void Sorted()
     {
         if (cbSort.SelectedItem is SortingControl selectedSorter)
         {
@@ -59,6 +124,9 @@ public partial class MainForm : Form
         }
         _bindingSource.ResetBindings(false);
     }
+    #endregion
+
+    #region Events
     private void OnClickNew(object? sender, EventArgs e)
     {
         var newPerson = new AddForm(_bindingSource);
@@ -89,4 +157,5 @@ public partial class MainForm : Form
             editForm.ShowDialog();
         }
     }
+    #endregion
 }
--- PeopleManagement/Program.cs
+++ PeopleManagement/Program.cs
@@ -6,26 +6,24 @@ namespace PeopleManagement;
 internal static class Program
 {
     public static List<Person> People { get; set; } = new();
-    private static void Initailize()
-    {
-        for (int k = 1; k <= 10; k++)
-        {
-            People.Add(new Person
-            {
-                No = k,
-                Name = $"Name {k}",
-                Age = (k * 22) % 7,
-            });
-        }
-    }
+    //private static void Initailize()
+    //{
+    //    for (int k = 1; k <= 10; k++)
+    //    {
+    //        People.Add(new Person
+    //        {
+    //            No = k,
+    //            Name = $"Name {k}",
+    //            Age = (k * 22) % 7,
+    //        });
+    //    }
+    //}
 
     [STAThread]
     static void Main()
     {
-        Initailize();
+        //Initailize();
 
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
         Application.Run(new Pages.MainForm());
     }
